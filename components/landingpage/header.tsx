4bd8c54b9fdf933274d997fce0bbd39ad8d647bd
root <root@Felipe>
2024-04-11 22:59:51 -0300

    landing page and routes

---
+++ components/landingpage/header.tsx
@@ -0,0 +1,7 @@
+export default function LandingPageHeader() {
+    return (
+      <div class="w-full h-20 bg-[#29323A]">
+        <img class="absolute ml-[50px] mt-[20px]" src="logo harpon (2).svg" />
+      </div>
+    );
+  }
--- components/product/ProductShelf.tsx
+++ components/product/ProductShelf.tsx
@@ -11,6 +11,9 @@ import { useOffer } from "$store/sdk/useOffer.ts";
 import { usePlatform } from "$store/sdk/usePlatform.tsx";
 import type { Product } from "apps/commerce/types.ts";
 import { mapProductToAnalyticsItem } from "apps/commerce/utils/productToAnalyticsItem.ts";
+import LandingPageFooter from "deco-sites/harpon-lp/components/landingpage/footer.tsx";
+import ImagesProducts from "deco-sites/harpon-lp/components/landingpage/body.tsx";
+import LandingPageHeader from "deco-sites/harpon-lp/components/landingpage/header.tsx";
 
 export interface Props {
   products: Product[] | null;
@@ -38,62 +41,10 @@ function ProductShelf({
   }
 
   return (
-    <div class="w-full container  py-8 hidden  flex-col gap-12 lg:gap-16 lg:py-10">
-      <Header
-        title={title || ""}
-        description={description || ""}
-        fontSize={layout?.headerfontSize || "Large"}
-        alignment={layout?.headerAlignment || "center"}
-      />
-
-      <div
-        id={id}
-        class="container grid grid-cols-[48px_1fr_48px] px-0 sm:px-5"
-      >
-        <Slider class="carousel carousel-center sm:carousel-end gap-6 col-span-full row-start-2 row-end-5">
-          {products?.map((product, index) => (
-            <Slider.Item
-              index={index}
-              class="carousel-item w-[270px] sm:w-[292px] first:pl-6 sm:first:pl-0 last:pr-6 sm:last:pr-0"
-            >
-              <ProductCard
-                product={product}
-                itemListName={title}
-                layout={cardLayout}
-                platform={platform}
-              />
-            </Slider.Item>
-          ))}
-        </Slider>
-
-        <>
-          <div class="hidden relative sm:block z-10 col-start-1 row-start-3">
-            <Slider.PrevButton class="btn btn-circle btn-outline absolute right-1/2 bg-base-100">
-              <Icon size={24} id="ChevronLeft" strokeWidth={3} />
-            </Slider.PrevButton>
-          </div>
-          <div class="hidden relative sm:block z-10 col-start-3 row-start-3">
-            <Slider.NextButton class="btn btn-circle btn-outline absolute left-1/2 bg-base-100">
-              <Icon size={24} id="ChevronRight" strokeWidth={3} />
-            </Slider.NextButton>
-          </div>
-        </>
-        <SliderJS rootId={id} />
-        <SendEventOnLoad
-          event={{
-            name: "view_item_list",
-            params: {
-              item_list_name: title,
-              items: products.map((product) =>
-                mapProductToAnalyticsItem({
-                  product,
-                  ...(useOffer(product.offers)),
-                })
-              ),
-            },
-          }}
-        />
-      </div>
+    <div>
+      <LandingPageHeader />
+      <ImagesProducts />
+      <LandingPageFooter />
     </div>
   );
 }
